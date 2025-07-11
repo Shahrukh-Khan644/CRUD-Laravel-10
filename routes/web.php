c2603c173ffa14c29b85ceedeeb3ed0820a7c9f2
Shahrukh <you@example.com>
2025-07-11 23:52:54 +0500

    added crud app

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-//route resource
-Route::resource('/posts', \App\Http\Controllers\PostController::class);
+Route::get('/', function () {
+    return view('welcome'); // or return 'It works!';
+});
